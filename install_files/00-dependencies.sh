162afca3e95cd9e4a83247a1bd4bf70eb277da71
moose <mustafa.ascha@gmail.com>
2018-06-29 15:37:21 -0400

    switch to java 10

--- install_files/00-dependencies.sh
+++ install_files/00-dependencies.sh
@@ -22,9 +22,14 @@ sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with passw
 #sudo apt-get -y install openjdk-8-jdk
 
 #see: http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
-sudo add-apt-repository ppa:webupd8team/java
+#sudo add-apt-repository ppa:webupd8team/java
+#sudo apt-get update
+#echo oracle-java9-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+#sudo apt-get install -y oracle-java9-installer oracle-java9-set-default
+
+sudo add-apt-repository ppa:linuxuprising/java
 sudo apt-get update
-echo oracle-java9-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-get install -y oracle-java9-installer oracle-java9-set-default
+sudo apt-get -y install oracle-java10-installer
+sudo apt-get -y install oracle-java-10-set-default
 
 
