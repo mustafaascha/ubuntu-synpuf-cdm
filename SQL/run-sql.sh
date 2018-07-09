2995c6ff79f522dda6a8aac8764c57bf49e69018
Mustafa Ascha <mustafa.ascha@gmail.com>
2018-07-09 10:13:26 -0400

    Update run-sql.sh

--- SQL/run-sql.sh
+++ SQL/run-sql.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-#source activate py27
-
-#for i in {1..20}
-#do
-#    python python_etl/etlCDMV5.py $i
-#done
-
-#http://www.zsh.org/mla/users/2002/msg00033.html
-#https://stackoverflow.com/questions/246215/how-can-i-list-files-with-their-absolute-path-in-linux
-
-#ls -d -1 /home/ETL-CMS/cache
-#psql -U postgres -f SQL/init-postgres.SQL
-#psql -U postgres -f SQL/create_CDMv5_tables.sql
-#psql -U postgres -f SQL/load_CDMv5_vocabulary.sql
-#psql -U postgres -f SQL/load_CDMv5_synpuf.sql
-#psql -U postgres -f SQL/create_CDMv5_constraints.sql
-#psql -U postgres -f SQL/create_CDMv5_indices.sql
-#psql -U postgres -f SQL/create_CDMv5_condition_era.sql
-#psql -U postgres -f SQL/create_CDMv5_drug_era_non_stockpile.sql
-
 #https://stackoverflow.com/questions/29161323/how-to-keep-associative-array-order-in-bash
 fls=(
 "init-postgres.sql"
@@ -41,20 +21,4 @@ do
   sudo -i -u postgres psql -U postgres -f $the_dir/SQL/${fls[$file]}
   echo "${fls[$file]} has been run"
 done
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
