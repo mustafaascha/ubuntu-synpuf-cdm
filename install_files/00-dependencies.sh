135d1b4c76960c019bb3fae9883867be80949ec4
Mustafa Ascha <mustafa.ascha@gmail.com>
2018-07-09 10:14:06 -0400

    Update 00-dependencies.sh

--- install_files/00-dependencies.sh
+++ install_files/00-dependencies.sh
@@ -18,15 +18,6 @@ echo 'systemctl start postgresql' | sudo bash
 echo 'setting postgres password to "password", change ASAP!'
 sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'password';"
 
-#export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
-#sudo apt-get -y install openjdk-8-jdk
-
-#see: http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
-#sudo add-apt-repository ppa:webupd8team/java
-#sudo apt-get update
-#echo oracle-java9-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-#sudo apt-get install -y oracle-java9-installer oracle-java9-set-default
-
 sudo add-apt-repository ppa:linuxuprising/java
 sudo apt-get update
 sudo apt-get -y install oracle-java10-installer
